5e1111bda81957ef117fec3b7d3bfe26f3d7454f
Marcus Medina <medina.jobb@outlook.com>
2021-11-03 08:45:49 +0100

    It freaking works!!!!

--- AutoLandingPageGenerator/Form1.cs
+++ AutoLandingPageGenerator/Form1.cs
@@ -13,6 +13,8 @@
     public partial class Form1 : Form
     {
         private int pageId = 0;
+        private bool IsRefreshing = false;
+
         private readonly WebPage webPage = new();
         public Form1()
         {
@@ -26,40 +28,125 @@
 
         private void RefreshSection()
         {
+            IsRefreshing = true;
             if (pageId < 0) pageId = 0;
             if (pageId >= webPage.Sections.Count) pageId = webPage.Sections.Count - 1;
+
             var section = webPage.Sections[pageId];
             txName.Text = section.Name;
-            txTitle.Text = section.Title;
+            txTitle.Text = section.Label;
             txPadding.Text = section.Padding;
             txMargin.Text = section.Margin;
             cbFontFamily.Text = section.Font;
             txSize.Text = section.FontSize;
             btnBackColor.Text = section.BackColor;
-            btnBackColor.Text = section.ForeColor;
+            btForeColor.Text = section.ForeColor;
             btBackImage.Text = section.BackImage;
             ckStretch.Checked = section.Stretch;
             ckScroll.Checked = section.Scroll;
             ckRepeat.Checked = section.Repeat;
-            txText.Text= section.Text;
+            txText.Text = section.Text;
+            txHeight.Text = section.Height;
+            txWidth.Text = section.Width;
+            if (section.ArticlePicture != "")
+                pictureBox1.Image = Image.FromFile(section.ArticlePicture);
+            else
+                pictureBox1.Image = null;
+
+            bool txNameVisible = true;
+            bool txTitleVisible = true;
+            bool txPaddingVisible = true;
+            bool txMarginVisible = true;
+            bool cbFontFamilyVisible = true;
+            bool txSizeVisible = true;
+            bool btnBackColorVisible = true;
+            bool btnForeColorVisible = true;
+            bool btBackImageVisible = true;
+            bool ckStretchVisible = true;
+            bool ckScrollVisible = true;
+            bool ckRepeatVisible = true;
+            bool txTextVisible = true;
+            bool PictureVisible = false;
+            bool titleVisible = true;
+            comboBox1.Enabled = true;
+            comboBox1.SelectedIndex = comboBox1.FindString(section.SectionType.ToString());
+
+
+            switch (section.SectionType)
+            {
+                case SectionType.Main:
+                    txTextVisible = false;
+                    PictureVisible = false;
+                    titleVisible = false;
+                    comboBox1.Enabled = false;
+                    break;
+                case SectionType.Footer:
+                    PictureVisible = false;
+                    titleVisible = false;
+                    comboBox1.Enabled = false;
+                    break;
+                case SectionType.NotDefined:
+                    break;
+                case SectionType.ScrollingBanner:
+                    break;
+                case SectionType.CallToAction:
+                    break;
+                case SectionType.Text:
+                    break;
+                case SectionType.TextWithImageRight:
+                    PictureVisible = true;
+                    break;
+                case SectionType.TextWithImageLeft:
+                    PictureVisible = true;
+                    break;
+                case SectionType.TextAndButton:
+                    break;
+                case SectionType.ContactForm:
+                    break;
+                default:
+                    break;
+            }
+            txText.Width = PictureVisible ? 704 : 902;
+            txText.Left = section.SectionType == SectionType.TextWithImageLeft ? 209 : 13;
+            pictureBox1.Left = section.SectionType == SectionType.TextWithImageRight ? 721 : 13;
+
+            txName.Visible = txNameVisible;
+            txTitle.Visible = txTitleVisible;
+            txPadding.Visible = txPaddingVisible;
+            txMargin.Visible = txMarginVisible;
+            cbFontFamily.Visible = cbFontFamilyVisible;
+            txSize.Visible = txSizeVisible;
+            btnBackColor.Visible = btnBackColorVisible;
+            btForeColor.Visible = btnForeColorVisible;
+            btBackImage.Visible = btBackImageVisible;
+            ckStretch.Visible = ckStretchVisible;
+            ckScroll.Visible = ckScrollVisible;
+            ckRepeat.Visible = ckRepeatVisible;
+            txText.Visible = txTextVisible;
+            pictureBox1.Visible = PictureVisible;
+            lbTitle.Visible = titleVisible;
+            txTitle.Visible = titleVisible;
+            IsRefreshing = false;
         }
 
         private void UpdateSection()
         {
             var section = webPage.Sections[pageId];
             section.Name = txName.Text;
-            section.Title = txTitle.Text;
+            section.Label = txTitle.Text;
             section.Padding = txPadding.Text;
             section.Margin = txMargin.Text;
             section.Font = cbFontFamily.Text;
             section.FontSize = txSize.Text;
             section.BackColor = btnBackColor.Text;
-            section.ForeColor = btnBackColor.Text;
+            section.ForeColor = btForeColor.Text;
             section.BackImage = btBackImage.Text;
             section.Stretch = ckStretch.Checked;
             section.Scroll = ckScroll.Checked;
             section.Repeat = ckRepeat.Checked;
             section.Text = txText.Text;
+            section.Height = txHeight.Text;
+            section.Width = txWidth.Text;
             webPage.Sections[pageId] = section;
         }
 
@@ -75,27 +162,82 @@
             cbFontFamily.Items.Add("fantasy");
             cbFontFamily.Items.Add("ui-monospace");
             RefreshSection();
+
+            var types = System.Enum.GetNames(typeof(SectionType));
+            comboBox1.Items.AddRange(types);
         }
 
-        private void BtMoveLeft_Click(object sender, EventArgs e)
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            UpdateSection();
+            pageId++;
+            RefreshSection();
+
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
         {
             UpdateSection();
             pageId--;
             RefreshSection();
         }
 
-        private void BtMoveRight_Click(object sender, EventArgs e)
-        {
-            UpdateSection();
-            pageId++;
-            RefreshSection();
-        }
-
-        private void BtPreview_Click(object sender, EventArgs e)
+        private void btPreview_Click(object sender, EventArgs e)
         {
             UpdateSection();
             webPage.Generate(@".\");
         }
 
+        private void btnMoveRight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnBackColor_Click(object sender, EventArgs e)
+        {
+            var colSel = new ColorDialog();
+            colSel.Color = ColorTranslator.FromHtml(btnBackColor.Text);
+            colSel.ShowDialog();
+            btnBackColor.Text = ColorTranslator.ToHtml(colSel.Color);
+        }
+
+        private void BtForeColor_Click(object sender, EventArgs e)
+        {
+            var colSel = new ColorDialog();
+            colSel.Color = ColorTranslator.FromHtml(btForeColor.Text);
+            colSel.ShowDialog();
+            btForeColor.Text = ColorTranslator.ToHtml(colSel.Color);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!IsRefreshing)
+            {
+                var type = (SectionType)Enum.Parse(typeof(SectionType), comboBox1.Text);
+                webPage.Sections[pageId].SectionType = type;
+                RefreshSection();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            OpenFileDialog fileDlg = new()
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.jfif",
+                FileName = userFolder
+            };
+            fileDlg.ShowDialog();
+            webPage.Sections[pageId].ArticlePicture = fileDlg.FileName;
+            try
+            {
+                pictureBox1.Image = Image.FromFile(fileDlg.FileName);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error reading image");
+            }
+        }
     }
 }
--- AutoLandingPageGenerator/WebPage.cs
+++ AutoLandingPageGenerator/WebPage.cs
@@ -7,9 +7,10 @@ namespace AutoLandingPageGenerator
 {
     using System;
     using System.Collections.Generic;
-using System.Diagnostics;
+    using System.Diagnostics;
     using System.IO;
     using System.Text;
+    using System.Windows.Forms;
 
     internal class WebPage
     {
@@ -20,19 +21,23 @@ using System.Diagnostics;
             Sections.AddRange(
                 new WebSection[]
                 {
-                    new WebSection{SectionType=SectionType.Main,Name="Landing page"},
-                    new WebSection{SectionType=SectionType.Footer,Name="Footer"},
-                    new WebSection{SectionType=SectionType.NotDefined, Name="Section1"},
-                    new WebSection{SectionType=SectionType.NotDefined, Name="Section2"},
-                    new WebSection{SectionType=SectionType.NotDefined, Name="Section3"},
-                });
+                    new WebSection{SectionType=SectionType.Main,Name="Landing page", Label="Body", FontSize="12px", BackColor="#000",ForeColor="White", Padding="0px", Margin="0px", Height="100%"},
+                    new WebSection{SectionType=SectionType.Footer,Name="Footer", Label="Footer", Text="(C) 2021 by me!", FontSize="12px", BackColor="#000",ForeColor="White", Height="20px"},
+                    new WebSection{SectionType=SectionType.Text, Name="Section1",Label="Section1", Text="Some info", FontSize="12px", BackColor="#F00",ForeColor="White", Height="100%"},
+                    new WebSection{SectionType=SectionType.TextAndButton, Name="Section2",Label="Section2",Text="Some info", FontSize="12px", BackColor="#0F0",ForeColor="White", Height="100%"},
+                    new WebSection{SectionType=SectionType.Text, Name="Section3",Label="Section3",Text="Some info", FontSize="12px", BackColor="#00F",ForeColor="White", Height="100%"},
+                }); ;
         }
 
         internal void Generate(string filePath)
         {
             var html = new StringBuilder();
             var css = new StringBuilder();
+            var nav = new StringBuilder();
             html.AppendLine("<html lang=\"sv\">");
+            css.AppendLine(".whooper{min-height: 90%;}");
+            css.AppendLine(".content {flex-grow: 1;}");
+            WebSection footer = null;
             foreach (var item in Sections)
             {
                 switch (item.SectionType)
@@ -41,45 +46,132 @@ using System.Diagnostics;
                         html.AppendLine("<head>");
                         html.AppendLine("<title>" + item.Name + "</title>");
                         html.AppendLine("<meta charset=\"utf-8\"/>");
+                        html.AppendLine("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">");
                         html.AppendLine("</head>");
                         html.AppendLine("<body>");
+                        html.AppendLine("<div class=\"whooper\">");
+                        html.AppendLine("<header><h1>" + item.Name + "</h1>");
+                        html.AppendLine("<nav>{0}<nav></header>");
+                        css.AppendLine("html,body");
+                        css.AppendLine("{");
                         break;
                     case SectionType.Footer:
-                        html.AppendLine("<footer>");
-                        html.AppendLine(item.Text);
-                        html.AppendLine("</footer>");
+                        css.AppendLine("footer");
+                        css.AppendLine("{");
+                        footer = item;
                         break;
                     case SectionType.NotDefined:
                         html.AppendLine("<p>");
-                        html.AppendLine(item.Text);
+                        html.AppendLine(item.Text.Replace("\r\n", "<br>"));
                         html.AppendLine("</p>");
                         break;
                     case SectionType.ScrollingBanner:
+                        html.AppendLine("<a name=Link" + item.Label + ">");
+                        nav.AppendLine("<a href=#" + item.Label + ">" + item.Name + "</a>");
+                        css.AppendLine("#" + item.Label);
+                        css.AppendLine("{");
                         break;
                     case SectionType.CallToAction:
+                        html.AppendLine("<a name=Link" + item.Label + ">");
+                        nav.AppendLine("<a href=#" + item.Label + ">" + item.Name + "</a>");
+                        css.AppendLine("#" + item.Label);
+                        css.AppendLine("{");
                         break;
                     case SectionType.Text:
+                        css.AppendLine("#" + item.Label);
+                        nav.AppendLine("<a href=#" + item.Label + ">" + item.Name + "</a>");
+                        css.AppendLine("{");
+                        html.AppendLine("<a name=Link" + item.Label + ">");
+                        html.AppendLine("<div id=" + item.Label + ">");
+                        html.AppendLine("<h1>" + item.Name + "</h1>");
+                        html.AppendLine(item.Text.Replace("\r\n", "<br>"));
+                        html.AppendLine("</div>");
                         break;
                     case SectionType.TextWithImageRight:
+                        html.AppendLine("<a name=Link" + item.Label + ">");
+                        nav.AppendLine("<a href=#" + item.Label + ">" + item.Name + "</a>");
+                        html.AppendLine("<div id=" + item.Label + ">");
+                        html.AppendLine("<h1>" + item.Name + "</h1>");
+                        if (item.ArticlePicture != "") html.AppendLine("<img src=\"" + item.ArticlePicture + "\">");
+                        html.AppendLine(item.Text.Replace("\r\n", "<br>"));
+                        html.AppendLine("</div>");
+                        css.AppendLine("#" + item.Label + " img{float:right; height:75%}");
+                        css.AppendLine("#" + item.Label);
+                        css.AppendLine("{");
                         break;
                     case SectionType.TextWithImageLeft:
+                        html.AppendLine("<a name=Link" + item.Label + ">");
+                        nav.AppendLine("<a href=#" + item.Label + ">" + item.Name + "</a>");
+                        html.AppendLine("<div id=" + item.Label + ">");
+                        html.AppendLine("<h1>" + item.Name + "</h1>");
+                        if (item.ArticlePicture != "") html.AppendLine("<img src=\"" + item.ArticlePicture + "\">");
+                        html.AppendLine(item.Text.Replace("\r\n", "<br>"));
+                        html.AppendLine("</div>");
+                        css.AppendLine("#" + item.Label + " img{float:left; height:75%}");
+                        css.AppendLine("#" + item.Label);
+                        css.AppendLine("{");
                         break;
                     case SectionType.TextAndButton:
+                        html.AppendLine("<a name=Link" + item.Label + ">");
+                        nav.AppendLine("<a href=#" + item.Label + ">" + item.Name + "</a>");
+                        html.AppendLine("<div id=" + item.Label + ">");
+                        html.AppendLine("<h1>" + item.Name + "</h1>");
+                        html.AppendLine(item.Text.Replace("\r\n", "<br>"));
+                        html.AppendLine("</div>");
+                        css.AppendLine("#" + item.Label);
+                        css.AppendLine("{");
                         break;
                     case SectionType.ContactForm:
+                        html.AppendLine("<a name=Link" + item.Label + ">");
+                        nav.AppendLine("<a href=#" + item.Label + ">" + item.Name + "</a>");
+                        html.AppendLine("<div id=" + item.Label + ">");
+                        html.AppendLine("<h1>" + item.Name + "</h1>");
+                        html.AppendLine(item.Text.Replace("\r\n", "<br>"));
+                        html.AppendLine("</div>");
+                        css.AppendLine("#" + item.Label);
+                        css.AppendLine("{");
                         break;
                     default:
+                        html.AppendLine("<a name=Link" + item.Label + ">");
+                        nav.AppendLine("<a href=#" + item.Label + ">" + item.Name + "</a>");
+                        html.AppendLine("<div id=" + item.Label + ">");
+                        html.AppendLine("<h1>" + item.Name + "</h1>");
+                        html.AppendLine(item.Text.Replace("\r\n", "<br>"));
+                        html.AppendLine("</div>");
+                        css.AppendLine("#" + item.Label);
+                        css.AppendLine("{");
                         break;
                 }
+
+                if (item.SectionType != SectionType.NotDefined)
+                {
+                    css.AppendLine(AddIfNotEmpty("Padding", item.Padding));
+                    css.AppendLine(AddIfNotEmpty("Margin", item.Margin));
+                    css.AppendLine(AddIfNotEmpty("background-color", item.BackColor));
+                    css.AppendLine(AddIfNotEmpty("font-size", item.FontSize));
+                    css.AppendLine(AddIfNotEmpty("color", item.ForeColor));
+                    css.AppendLine(AddIfNotEmpty("width", item.Width));
+                    css.AppendLine(AddIfNotEmpty("height", item.Height));
+                    css.AppendLine(AddIfNotEmpty("font-family", item.Font));
+                    css.AppendLine("}");
+                }
             }
+            html.AppendLine("</div>");
+            html.AppendLine("<footer>");
+            html.AppendLine(footer.Text);
+            html.AppendLine("</footer>");
             html.AppendLine("</body>");
             html.AppendLine("</html>");
-
-            var htmlFile = SaveHTMLFile(filePath, html);
+            var htmlFile = SaveHTMLFile(filePath, html, nav);
             SaveCSSFile(filePath, css);
 
             OpenWebpage(htmlFile);
 
+        }
+
+        private string AddIfNotEmpty(string tag, string value)
+        {
+            return string.IsNullOrEmpty(value) ? "" : tag + ":" + value + ";";
         }
 
         private static void SaveCSSFile(string filePath, StringBuilder css)
@@ -88,17 +180,18 @@ using System.Diagnostics;
             File.WriteAllText(cssFile, css.ToString());
         }
 
-        private static string SaveHTMLFile(string filePath, StringBuilder html)
+        private static string SaveHTMLFile(string filePath, StringBuilder html, StringBuilder nav)
         {
             var htmlFile = Path.Combine(filePath, "index.html");
-            File.WriteAllText(htmlFile, html.ToString());
+            var htmlNav = string.Format(html.ToString(), nav.ToString());
+            File.WriteAllText(htmlFile, htmlNav);
             return htmlFile;
         }
 
         private static void OpenWebpage(string htmlFile)
         {
             // Snatched from https://stackoverflow.com/a/4580317
-            Process myProcess = new Process();
+            Process myProcess = new();
             try
             {
                 // true is the default, but it is important not to set it to false
